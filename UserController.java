c6ba09aacccc933b1d4ae11f350e811ad1cf177b
younghyeonjeon <jyh1355@naver.com>
2020-02-12 17:07:04 +0900

    Add User Controller [Login]

---
+++ UserController.java
@@ -0,0 +1,10 @@
+@Controller
+public class UserController{
+	//로그인 페이지
+
+	@RequestMapping("/user/login.do")
+	public String login(){
+	      logger.info("경로:login");
+                  return "user/login";
+	}
+}
