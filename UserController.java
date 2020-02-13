2a799b8df0eec5e5daa2745aebbf0bd773de6e79
younghyeonjeon <jyh1355@naver.com>
2020-02-13 16:17:42 +0900

    Add UserController [select]

--- UserController.java
+++ UserController.java
@@ -29,6 +29,11 @@ public class UserController{
 	      logger.info("경로:delete");
                   return "user/delete";
 	}
-
-
+	
+	//회원 조회 페이지로 이동-develop 브랜치에서 작업 
+	@RequestMapping("/user/select.do")
+	public String select(){
+	      logger.info("경로:select");
+                  return "user/select";
+	}
 }
